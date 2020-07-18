85a70bdea40b1aa4dd67dba4bfd84b6871290e36
s-blais <stephen@s-blais.com>
2020-07-18 16:32:15 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,8 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create(post_params)
-    redirect_to post_path(@post)
+    # instead of responding with a redirect to the show page, conjure JSON object of the new post with the serializer:
+    render json: @post, status: 201
   end
 
   def edit
